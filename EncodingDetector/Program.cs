1150fcad86e9adb1495283c19693d4152c5e1b9b
caplanet_hsw <hsw@caplanet.kr>
2023-12-27 14:33:33 +0900

    init

---
+++ EncodingDetector/Program.cs
@@ -0,0 +1,31 @@
+﻿using UtfUnknown;
+
+namespace EncodingDetecter;
+
+internal class Program
+{
+	static void Main(string[] args)
+	{
+		try
+		{
+			if (args.Length == 0)
+				throw new Exception();
+
+			if (!File.Exists(args[0]))
+				throw new Exception();
+
+			var result = CharsetDetector.DetectFromFile(args[0]);
+			if (result.Detected == null)
+				throw new Exception();
+			if (result.Detected.Encoding.CodePage == 1256)
+				throw new Exception();
+
+			Console.Write(result.Detected.EncodingName);
+			return;
+		}
+		catch
+		{
+			Console.Write("utf-8");
+		}
+	}
+}
